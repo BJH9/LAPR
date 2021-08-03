b3b788cad138bf7c5b052782d41c5907d8dfae6c
BJH9 <tfzangin9@gmail.com>
2021-08-03 18:00:22 +0900

    tab

--- app/src/main/java/edu/handong/csee/lapr/APRMain.java
+++ app/src/main/java/edu/handong/csee/lapr/APRMain.java
@@ -13,18 +13,12 @@ public class APRMain {
 	
 	
 	
-	public static void main(String[] args) {
-
-		 
+	public static void main(String[] args) {		 
         APRMain runner = new APRMain();
         runner.run(args);
-
-
-
 	}
 
-	private void run(String[] args) {
-		
+	private void run(String[] args) {		
 		projectDirectoryPath = args[0];
 		testcaseDirectoryPath = args[1];
 		
@@ -34,24 +28,17 @@ public class APRMain {
 		Patcher patcher = new Patcher();
 
 		//FaultLocalization: projectDirectoryPath에 테스트케이스를 적용하는 과정을 거쳐 faultDirectoryPath faultFilePath와 faultLocationLineNumber생성한다.
-		
 		faultLocalizer.excute(projectDirectoryPath, testcaseDirectoryPath);
 		faultFilePath = faultLocalizer.getFaultFilePath();
 		faultLocationLineNumber = faultLocalizer.getFaultLocationLineNumber();
 		
-		
 		//PatchGeneration: faultDirectoryPath,  faultFilePath, faultLocationLineNumber위치에 결함에 대한 testcase들을 적용해 패치들을 생성한다. candidate patch
 		candidatePatches = patchGenerator.getPatches(faultDirectoryPath, faultFilePath, faultLocationLineNumber);
-		
 		
 		//PatchValidation: candidate patch에 테스트케이스를 적용해 최종적으로 final patch를 찾는다.
 		finalPatch = patchValidator.getFinalPatches(candidatePatches);
 		
-		
 		//Patch apply: faultFilePath의 오리지널 파일의 결함코드에 correct patch를 적용시켜 최종 수정한다.
 		patcher.fixFaultCode(finalPatch);
-
-		
-		
 	}
 }
