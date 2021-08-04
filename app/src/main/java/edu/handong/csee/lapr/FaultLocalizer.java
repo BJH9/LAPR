3be1e375cfba5319d2d1d628ae6a9c3679f96f66
BJH9 <tfzangin9@gmail.com>
2021-08-04 16:49:42 +0900

    faultLocalize coding

--- app/src/main/java/edu/handong/csee/lapr/FaultLocalizer.java
+++ app/src/main/java/edu/handong/csee/lapr/FaultLocalizer.java
@@ -1,19 +1,38 @@
 package edu.handong.csee.lapr;
+import java.io.File;
 
 public class FaultLocalizer {
+	private String faultDirectoryPath;
+	private String faultFilePath;
+	private String faultLocationLineNumber;
 
-	public void excute(projectdirectorypath, testcasepath) {
+	public void excute(String projectDirectoryPath, String testCasePath) {
 		//테스트케이스를 projectdirectorypath에 적용한다.
 		//faultfilepath와 faultlocationlinenumber를 출력한다.
+		
+		String a = testCasePath;
+		File directory = new File(projectDirectoryPath);
+		String[] contents = directory.list();
+		File file = null;
+		
+		for(String content : contents) {
+			file = new File(directory.getAbsolutePath() + File.separator + content);
+			
+			if(file.isDirectory()) {
+				excute(file.getAbsolutePath(), a);
+			} else{
+				//test
+			}
+		}
 		
 	}
 	
 	public int getFaultFilePath() {
-		
+		return faultFilePath;
 	}
 	
 	public int getFaultLocationLineNumber() {
-		
+		return faultLocationLineNumber;
 	}
 	
 }
