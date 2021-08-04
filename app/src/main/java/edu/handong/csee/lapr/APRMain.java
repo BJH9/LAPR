82457187948f999f61620b175bbaa2eee01fbcd0
BJH9 <tfzangin9@gmail.com>
2021-08-04 13:57:39 +0900

    apply cli on apr

--- app/src/main/java/edu/handong/csee/lapr/APRMain.java
+++ app/src/main/java/edu/handong/csee/lapr/APRMain.java
@@ -1,8 +1,24 @@
 package edu.handong.csee.lapr;
-public class APRMain {
+import org.apache.commons.cli.Options;
 
+import edu.handong.csee.lapr.FaultLocalizer;
+import edu.handong.csee.lapr.PatchGenerator;
+import edu.handong.csee.lapr.PatchValidator;
+import edu.handong.csee.lapr.Patcher;
+
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+
+public class LAPR {
 	private String projectDirectoryPath;
-	private String testcaseDirectoryPath;
+	private String testCaseDirectoryPath;
+	private boolean help;
+	
+	private String projectDirPath;
+	private String testCasePath;
 	
 	private String faultDirectoryPath;
 	private String faultFilePath;
@@ -10,25 +26,34 @@ public class APRMain {
 	
 	private String[] candidatePatches;
 	private String finalPatch;
-	
-	
-	
-	public static void main(String[] args) {		 
-        APRMain runner = new APRMain();
-        runner.run(args);
-	}
 
-	private void run(String[] args) {		
-		projectDirectoryPath = args[0];
-		testcaseDirectoryPath = args[1];
+	public static void main(String[] args) {
+		LAPR runner = new LAPR();
+		runner.run(args);
+	}
+	
+	private void run(String[] args) {
+		Options options = createOptions();
+		
+		if(parseOption(options, args)) {
+			if(help) {
+				printHelp(options);
+				return;
+			}
+			
+			projectDirPath = projectDirectoryPath;
+			testCasePath = testCaseDirectoryPath;
+			
+			
+		}
 		
 		FaultLocalizer faultLocalizer = new FaultLocalizer();
 		PatchGenerator patchGenerator = new PatchGenerator();
 		PatchValidator patchValidator = new PatchValidator();
 		Patcher patcher = new Patcher();
-
+		
 		//FaultLocalization: projectDirectoryPath에 테스트케이스를 적용하는 과정을 거쳐 faultDirectoryPath faultFilePath와 faultLocationLineNumber생성한다.
-		faultLocalizer.excute(projectDirectoryPath, testcaseDirectoryPath);
+		faultLocalizer.excute(projectDirPath, testCasePath);
 		faultFilePath = faultLocalizer.getFaultFilePath();
 		faultLocationLineNumber = faultLocalizer.getFaultLocationLineNumber();
 		
@@ -41,4 +66,55 @@ public class APRMain {
 		//Patch apply: faultFilePath의 오리지널 파일의 결함코드에 correct patch를 적용시켜 최종 수정한다.
 		patcher.fixFaultCode(finalPatch);
 	}
+	
+	private boolean parseOption(Options options, String[] args) {
+		CommandLineParser parser = new DefaultParser();
+		
+		try {
+			CommandLine cmd = parser.parse(options, args);
+			
+			projectDirectoryPath = cmd.getOptionValue("f");
+			testCaseDirectoryPath = cmd.getOptionValue("t");
+			help = cmd.hasOption("h");
+		} catch(Exception e) {
+			printHelp(options);
+			return false;
+		}
+		
+		return true;
+		
+		
+	}
+	
+	private Options createOptions() {
+		Options options = new Options();
+		
+		options.addOption(Option.builder("f").longOpt("projectDirectoryPath")
+				.desc("set a path of a fault directory")
+				.hasArg()
+				.argName("Path name of fault directory")
+				.required()
+				.build());
+		
+		options.addOption(Option.builder("t").longOpt("testCaseDirectoryPath")
+				.desc("set a path of a testCasePath")
+				.hasArg()
+				.argName("Path name of test case")
+				.required()
+				.build());
+		
+		options.addOption(Option.builder("h").longOpt("help")
+				.desc("Help")
+				.build());
+		
+		return options;
+	}
+	
+	private void printHelp(Options options) {
+		HelpFormatter formatter = new HelpFormatter();
+		String header = "APR Test";
+		String footer = "\nPlease report issues";
+		formatter.printHelp("APR", header, options, footer, true);
+	}
+	
 }
